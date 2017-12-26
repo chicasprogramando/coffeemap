a0f0a04a08a68fb5ea5afb8ebf808e1e397b29ca
Gian F. Olivieri <gian.olivieri@gmail.com>
2017-12-26 19:40:56 -0300

    nicer Storybook

--- src/pages/Storybook/Storybook.vue
+++ src/pages/Storybook/Storybook.vue
@@ -2,13 +2,42 @@
 
 <template>
 	<div>
-		<h2>{{ title }}</h2>
-        <icon name="heart"/>
-        <icon name="twitter"/>
-        <visual-rating :value="4" />
-        <visual-rating :value="3" />
-        <div><input-dropdown /></div>
-        <div><input-dropdown :compact="true"/></div>
+        <h1>{{ title }}</h1>
+
+        <div class="component">
+            <h2>Icon</h2>
+            <h3>Normal</h3>
+            <icon name="heart"/>
+            <icon name="wifi"/>
+            <icon name="twitter"/>
+            <icon name="filter"/>
+
+            <h3>With color</h3>
+            <icon name="heart" color="#C00"/>
+            <icon name="wifi" color="#C00"/>
+            <icon name="twitter" color="#C00"/>
+            <icon name="filter" color="#C00"/>
+
+            <h3>Variable Size</h3>
+
+            <icon name="twitter" color="#F0C" :size="100" />
+            <icon name="twitter" color="#F0C" :size="50" />
+            <icon name="twitter" color="#F0C" :size="20" />
+
+            <h3>With circle</h3>
+            <icon name="filter" color="#FFF" circle="blueviolet"/>
+
+        </div>
+        <div class="component">
+            <h2>Visual Rating</h2>
+            <visual-rating :value="4" />
+            <visual-rating :value="3" />
+        </div>
+        <div class="component">
+            <h2>Input Dropdown</h2>
+            <div><input-dropdown /></div>
+            <div><input-dropdown :compact="true"/></div>
+        </div>
 	</div>
 </template>
 
@@ -17,8 +46,11 @@
 h2 {
     display: block;
     font-size: 30px;
+    border-bottom: 2px solid #ccc
 }
-
+.component {
+    padding: 16px;
+}
 span {
     display: block;
     font-size: 15px;
