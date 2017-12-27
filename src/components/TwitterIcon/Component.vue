7ce1408823452e0379f7db18650839a02fa9214f
Paola G <paolacgz@gmail.com>
2017-12-27 11:42:38 -0300

    Added TwitterIcon

---
+++ src/components/TwitterIcon/Component.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+       <img src="../assets/twitter.svg">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'component',
+  data () {
+      return {
+          iconName: 'TwitterIcon',
+      }
+  }
+}
+</script>
---
+++ src/components/TwitterIcon/TwitterIcon.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="twitter">
+      <img src="../assets/twitter.svg">
+      <Component v-bind:id='></Component>
+  </div>
+</template>
+
+<script>
+import Component from 'Component.vue'
+export default {
+  name: 'twitter',
+  props: ['name'],
+  components: {
+      Component
+  }
+}
+</script>
+
+<style scoped>
+    #twitter {
+        width: 100px;
+        height: 100px;
+        margin: 0;
+        padding: 40px;
+        background-color: blue;
+    }
+</style>
