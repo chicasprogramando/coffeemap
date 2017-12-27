4a1fe14b3afd66bddfd0fd9ef9d7589913ef8296
Paola G <paolacgz@gmail.com>
2017-12-27 19:24:30 -0300

    Added WiFiIcon

---
+++ src/components/WiFiIcon/WiFiIcon.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="wifi">
+      <img src="../assets/wifi.svg">
+      <Component v-bind:id='></Component>
+  </div>
+</template>
+
+<script>
+import Component from 'Component.vue'
+export default {
+  name: 'wifi',
+  props: ['name'],
+  components: {
+      Component
+  }
+}
+</script>
+
+<style scoped>
+    #wifi {
+        width: 100px;
+        height: 100px;
+        margin: 20px;
+        padding: 40px;
+        background-color: white;
+    }
+</style>
