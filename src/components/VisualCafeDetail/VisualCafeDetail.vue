c926879ac5b7d1d28c2eb8288372e9f0cbe3307a
Luciana Mendez <dg.lucianamendez@gmail.com>
2018-01-14 19:15:40 -0300

    VisualCafeDetail improved

--- src/components/VisualCafeDetail/VisualCafeDetail.vue
+++ src/components/VisualCafeDetail/VisualCafeDetail.vue
@@ -1,9 +1,29 @@
 <template>
-	<div>
-    <h1>{{title}}</h1>
-    <pre>
-      {{coffee}}
-    </pre>
+	<div :class="$style.mobile">
+    <app-header></app-header>
+    <div :class="$style.image"></div>
+    <div :class="$style.wrapper">      
+      <p :class="$style.coffeeName">{{coffee.name}}</p>
+      <visual-rating :value="coffee.rating"/>
+      <p :class="$style.subtitle">Información del lugar</p>
+      <div>
+        <p :class="$style.subtitleSmall">Dirección</p>
+        <p :class="$style.information">{{coffee.address}}</p>
+        <p :class="$style.subtitleSmall">Lunes a viernes</p>
+        <p :class="$style.information">8 a 20 hs</p>
+      </div>
+      <p :class="$style.subtitle">Información de contacto</p>
+      <div>
+        <p :class="$style.subtitleSmall">Teléfono</p>
+        <p :class="$style.information">{{coffee.telephone}}</p>
+        <p :class="$style.subtitleSmall">Sitio web</p>
+        <p :class="$style.information">{{coffee.website}}</p>
+      </div>
+      <div :class="$style.icon">
+        <icon name="twitter" color="#FFF" circle="blueviolet" :size="50"/>
+        <icon name="facebook" color="#FFF" circle="blueviolet" :size="50"/>
+      </div>
+    </div>
     <VisualSliderItem :name="coffee.name"
     :web="coffee.website" :rating="coffee.rating"
     :address="coffee.address"
@@ -12,31 +32,71 @@
 </template>
 
 <script>
-import VueTypes from 'vue-types';
-import VisualSliderItem from '../VisualSliderItem/VisualSliderItem';
+import VueTypes from "vue-types"
+import VisualSliderItem from "../VisualSliderItem/VisualSliderItem";
+import AppHeader from "../AppHeader/AppHeader";
+import VisualRating from "../../components/VisualRating/VisualRating";
+import Icon from "../Icon/Icon";
 
 export default {
   name: "visualCafeDetail",
-  components:{
+  components: {
     VisualSliderItem,
-
+    AppHeader,
+    VisualRating,
+    Icon
   },
   props: {
-    coffee: VueTypes.object,
+    coffee: VueTypes.object
   },
   data() {
-		return {
-			title: 'VisualCafeDetail'
-		};
-	},
-  methods: {
+    return {
+      title: "VisualCafeDetail"
+    };
   },
-  computed: {
-  },
-  mounted() {
-  }
+  methods: {},
+  computed: {},
+  mounted() {}
 };
 </script>
 
 <style module>
+.mobile {
+  width: 320px;
+}
+.wrapper {
+  padding: 30px;
+  background-color: white;
+}
+.image {
+  background-image: url("../../assets/cafevisualslideitem.jpg");
+  height: 80px;
+}
+
+.coffeeName {
+  font-weight: bolder;
+  font-size: 15pt;
+  color: rgb(93, 93, 93);
+}
+
+.subtitle {
+  font-weight: bolder;
+  color: blueviolet;
+  font-size: 10pt;
+}
+
+.subtitleSmall {
+  font-size: 9pt;
+  color: grey;
+}
+
+.information {
+  font-size: 10pt;
+  font-weight: bolder;
+  color: rgb(90, 90, 90);
+}
+
+.icon{
+  display:flex;
+}
 </style>
