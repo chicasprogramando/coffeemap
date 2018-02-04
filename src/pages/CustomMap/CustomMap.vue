69764184256a81499623705b8b01ef51e9ec3ac5
puribey <kenshipichu@gmail.com>
2018-02-04 13:31:47 -0300

    round 3 - data transfer trial

--- src/pages/CustomMap/CustomMap.vue
+++ src/pages/CustomMap/CustomMap.vue
@@ -7,7 +7,7 @@
         :center="{lat: -34.586347, lng: -58.431469}"
         @coffeClick="handleCoffeClick"
         />
-       <visualSlider/>
+       <visualSlider @coffeClick="handleCoffeClick"/>
         <VisualCafeDetail :coffee="coffee" v-if="coffee"/>
         <input-dropdown @input="changeNeighborhood" class="seconddropdown" :compact="true" :location="neighborhood"></input-dropdown>
     </div>
