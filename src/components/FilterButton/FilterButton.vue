c28d51f461c1e93a0588f1423f8c86b5fc6b60e9
Luciana Mendez <dg.lucianamendez@gmail.com>
2018-02-12 02:48:22 -0300

    restore prop to FilterButton

--- src/components/FilterButton/FilterButton.vue
+++ src/components/FilterButton/FilterButton.vue
@@ -2,7 +2,7 @@
     <button  :class="[$style.initial, status]" v-on:click="changeStatus" :style="defineSize">
           <icon  v-if="isActive" color="#7971D9" :name="name" :size="iconSize"/>
           <icon  v-else color="white" :name="name" :size="iconSize"/>
-          <label :class="[$style.initialLabel, labelStatus]"></label>
+          <label :class="[$style.initialLabel, labelStatus]">{{ buttonText }}</label>
     </button>
 </template>
 
@@ -14,6 +14,7 @@ export default {
   name: "FilterButton",
   props: {
     name: VueTypes.string.isRequired,
+    buttonText: VueTypes.string,
     iconSize: VueTypes.number.def(100),
     buttonSize: VueTypes.number.def(120)
   },
