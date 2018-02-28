b7a1f283b3bf51ea6cd601ac6bf9ac2e6d2f1505
Alan Gabriel Dawidowicz <alan.dawidowicz@mediamonks.com>
2018-02-28 14:11:31 -0300

    Clean rename some actions and mutations // Add visited coffee hover icon

--- src/pages/CustomMap/CustomMap.js
+++ src/pages/CustomMap/CustomMap.js
@@ -1,84 +1,80 @@
-import { TweenMax, Expo } from 'gsap';
-import Map from '../../components/Map/Map';
-import VisualSlider from '../../components/VisualSlider/VisualSlider';
-import VisualCafeDetail from '../../components/VisualCafeDetail/VisualCafeDetail';
-import InputDropdown from '../../components/InputDropdown/InputDropdown.vue';
+import { TweenMax, Expo } from "gsap";
+import Map from "../../components/Map/Map";
+import VisualSlider from "../../components/VisualSlider/VisualSlider";
+import VisualCafeDetail from "../../components/VisualCafeDetail/VisualCafeDetail";
+import InputDropdown from "../../components/InputDropdown/InputDropdown.vue";
 import ButtonIcon from "../../components/ButtonIcon/ButtonIcon.vue";
 import ButtonGeneral from "../../components/ButtonGeneral/ButtonGeneral";
 import AllFilters from "../../components/AllFilters/AllFilters.vue";
 
-
-
 export default {
-	name: 'customMap',
-	components: {
-		MapWrapper: Map,
-		VisualCafeDetail,
-		VisualSlider,
-		InputDropdown,
-		ButtonIcon,
-		ButtonGeneral,
-		AllFilters
-	},
-	data() {
-		return {
-			title: 'CustomMap',
-			coffee: null,
-			modalIn: false,
-			modalOut: false
-		};
-	},
-	methods: {
-		handleCoffeeClick : function(coffee) {
-			this.coffee = coffee; // TODO: Hacer que se obtenga el café desde un getter
-			console.log('Path click #2');
-			this.$router.push({name: 'coffee', params:{id: coffee.id}});
-		},
-		changeNeighborhood(value) {
-				this.$store.dispatch('actionTest', value);
-		},
-		closeDetail() {
-			this.coffee = null;
-			this.$router.push({name: 'map'});
-		},
-		handleMarkerClick : function(coffee) {
-			let idx = this.coffees.indexOf(coffee);
-			this.$refs.slider.slideTo(idx);
-		},
+  name: "customMap",
+  components: {
+    MapWrapper: Map,
+    VisualCafeDetail,
+    VisualSlider,
+    InputDropdown,
+    ButtonIcon,
+    ButtonGeneral,
+    AllFilters
+  },
+  data() {
+    return {
+      title: "CustomMap",
+      coffee: null,
+      modalIn: false,
+      modalOut: false
+    };
+  },
+  methods: {
+    handleCoffeeClick: function(coffee) {
+      this.coffee = coffee; // TODO: Hacer que se obtenga el café desde un getter
+      this.$router.push({ name: "coffee", params: { id: coffee.id } });
+    },
+    changeNeighborhood(value) {
+      this.$store.dispatch("selectNeighborhood", value);
+    },
+    closeDetail() {
+      this.coffee = null;
+      this.$router.push({ name: "map" });
+    },
+    handleMarkerClick: function(coffee) {
+      let idx = this.coffees.indexOf(coffee);
+      this.$refs.slider.slideTo(idx);
+    },
 
-
-		handleClickFilter(filter){
-		  this.$store.dispatch('Filter', filter);
-		},
-		onEnter(el, done) {
-			TweenMax.from(el, 0.5, {
-				right:-150,
-			  autoAlpha: 0,
-			  //scale: 0.8,
-			  ease: Expo.easeInOut,
-			  onComplete: () => {
-				done();
-			  },
-			});
-		},
-		  onLeave(el, done) {
-			TweenMax.to(el, 0.7, {
-				right:-150,
-				autoAlpha: 0,
-			  //scale: 0.8,
-			  ease: Expo.easeInOut,
-			  onComplete: () => {
-				done();
-			  },
-			});
-		},
-	},
-	computed: {
-		neighborhood(){
-			return this.$store.state.neighborhood;
-		},
-		coffees(){
-			return this.$store.getters.getFilteredCoffes;
-		}
-	},
+    handleClickFilter(filter) {
+      this.$store.dispatch("Filter", filter);
+    },
+    onEnter(el, done) {
+      TweenMax.from(el, 0.5, {
+        right: -150,
+        autoAlpha: 0,
+        //scale: 0.8,
+        ease: Expo.easeInOut,
+        onComplete: () => {
+          done();
+        }
+      });
+    },
+    onLeave(el, done) {
+      TweenMax.to(el, 0.7, {
+        right: -150,
+        autoAlpha: 0,
+        //scale: 0.8,
+        ease: Expo.easeInOut,
+        onComplete: () => {
+          done();
+        }
+      });
+    }
+  },
+  computed: {
+    neighborhood() {
+      return this.$store.state.neighborhood;
+    },
+    coffees() {
+      return this.$store.getters.getFilteredCoffes;
+    }
+  }
 };
--- src/store/index.js
+++ src/store/index.js
@@ -1,68 +1,86 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import Vue from "vue";
+import Vuex from "vuex";
 import api from "@/api";
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
-    state: {
-        coffees: [],
-        neighborhood: 'Palermo',
-        neighborhoods: [],
-        wifi: false,
-        coworking: false,
-        takeaway: false,
-        kitchen: false
+  state: {
+    coffees: [],
+    neighborhood: "Palermo",
+    neighborhoods: [],
+    wifi: false,
+    coworking: false,
+    takeaway: false,
+    kitchen: false
+  },
+  getters: {
+    getFilteredCoffes: state =>
+      state.coffees.filter(
+        coffee =>
+          coffee.zone === state.neighborhood &&
+          (!state.wifi || coffee.wifi === state.wifi) &&
+          (!state.coworking || coffee.coworking === state.coworking) &&
+          (!state.kitchen || coffee.kitchen === state.kitchen) &&
+          (!state.takeaway || coffee.takeaway === state.takeaway)
+      ),
+    getCoffeeById: state => id => state.coffees.find(value => value.id === id)
+  },
+  mutations: {
+    // TODO: Usar mayúsculas a la hora de hacer mutaciones
+    updateNeighborhood(state, payload) {
+      state.neighborhood = payload;
     },
-    getters: {
-        getFilteredCoffes: state => state.coffees.filter(
-            coffee => (
-                coffee.zone === state.neighborhood &&
-                (!state.wifi || coffee.wifi === state.wifi) &&
-                (!state.coworking || coffee.coworking === state.coworking) &&
-                (!state.kitchen || coffee.kitchen === state.kitchen) &&
-                (!state.takeaway || coffee.takeaway === state.takeaway)
-            )
-        ),
-        getCoffeeById: state => id => state.coffees.find(value => value.id === id),
+    updateFilter(state, filter) {
+      state[filter] = !state[filter];
+      // console.log(JSON.stringify(state));
     },
-    mutations:{
-        // TODO: Usar mayúsculas a la hora de hacer mutaciones
-        mutateNeighborhood(state, payload) {
-            state.neighborhood = payload;
-        },
-        mutateFilter(state,filter) {
-            state[filter]=!state[filter];
-            // console.log(JSON.stringify(state));
-        },
-        setCoffees(state, payload) {
-            state.coffees = payload;
-        },
-        setNeighborhoods(state, payload) {
-            state.neighborhoods = payload;
-        },
+    setCoffees(state, payload) {
+      state.coffees = payload;
     },
-    actions: {
-        actionTest({commit}, payload) {
-                commit('mutateNeighborhood', payload);
-        },
-        Filter({commit}, filter) {
-            commit('mutateFilter',filter);
-        },
-        fetchCoffees({commit}) {
-            api
-            .getCoffees()
-            .then(data => {commit('setCoffees', data)})
-            .catch(e => console.error(e));
-        },
-        fetchNeighborhoods({commit}){
-            api
-            .getNeighborhoods()
-            .then(data => {commit('setNeighborhoods', data)})
-            .catch(e => console.error(e));
+    setNeighborhoods(state, payload) {
+      state.neighborhoods = payload;
+    },
+    coffeeVisited(state, payload) {
+      const updatedCoffees = state.coffees.map(coffee => {
+        if (coffee.id === payload) {
+          coffee.visited = true;
         }
-     }
-    });
+
+        return coffee;
+      });
+
+      state.coffees = updatedCoffees;
+    }
+  },
+  actions: {
+    updateCoffeesVisited({ commit }, payload) {
+      commit("coffeeVisited", payload);
+    },
+    selectNeighborhood({ commit }, payload) {
+      commit("updateNeighborhood", payload);
+    },
+    Filter({ commit }, filter) {
+      commit("updateFilter", filter);
+    },
+    fetchCoffees({ commit }) {
+      api
+        .getCoffees()
+        .then(data => {
+          commit("setCoffees", data);
+        })
+        .catch(e => console.error(e));
+    },
+    fetchNeighborhoods({ commit }) {
+      api
+        .getNeighborhoods()
+        .then(data => {
+          commit("setNeighborhoods", data);
+        })
+        .catch(e => console.error(e));
+    }
+  }
+});
 
 export default store;
 
